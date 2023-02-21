7b4d42f4fef6c65312ba76874dc5bc9d61ff1b1a
yaruangr <125121426+yaruangr@users.noreply.github.com>
2023-02-21 22:53:23 +0700

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,23 @@
+
+
+
+
+#include    "libft.h"
+#include    <stdio.h>
+
+char    ft_strnstr (const char *big, const char *little, size_t len)
+{
+    
+
+
+}
+
+int main(void)
+{
+    const char *big = "Apple Papaya"
+    const char *little = "banana orange"
+    size_t = 3
+
+    printf("%d\n, ft_strnstr(*big, *little, size_t));
+    return (0);
+}
