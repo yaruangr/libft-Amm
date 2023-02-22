568256d011495cdc774d0111095612f39150b302
yaruangr <125121426+yaruangr@users.noreply.github.com>
2023-02-22 12:58:29 +0700

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,13 +18,15 @@ int ft_strncmp(char *str1, char *str2, int c)
 
     i = 0;
     while(str1[i] != '\0' && str2[i] != '\0')
-    if(str1[i] == str2[i])
+    {
+        if(str1[i] == str2[i])
     {
         i++;
     }
     else if(str1[i] != str2[i] && c != i)
     {
         return (str1[i] - str2[i]);
+    }
     }
     return (0);
 }
