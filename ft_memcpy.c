d138eec3aa21fe9b223fec68b560bd8d04352a01
yaruangr <125121426+yaruangr@users.noreply.github.com>
2023-03-10 15:12:29 +0700

    Create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+void  *memcpy(void *dest, const void * src, size_t n)
+{
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+int main(void)
+{
+
+
+
+
+
+
+}
