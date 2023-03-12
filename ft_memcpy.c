d361bb2ea75c4cc1647bd88c5633abdbd375b217
yaruangr <125121426+yaruangr@users.noreply.github.com>
2023-03-12 22:02:33 +0700

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,26 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaruangr < yaruangr@student.42bangkok.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 21:49:39 by yaruangr          #+#    #+#             */
+/*   Updated: 2023/03/12 22:00:19 by yaruangr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "libft.h"
-#include <stdio.h>
-#include <string.h>
+#include    "libft.h"
+//#include    <stdio.h>
+//#include    <string.h>
 
-void  *memcpy(void *dest, const void * src, size_t n)
+void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-  
-  
-  
-  
-  
-  
-  
-  
+    unsigned char   *deststr;
+    unsigned char   *srcstr;
+
+    deststr = dest;
+    srcstr = (unsigned char*)src;
+        if (!dest && !src)
+            return (NULL);
+        while (n-- > 0)
+        {
+            *deststr++ = *srcstr++;
+        }
+    return (dest);
 }
 
-int main(void)
+/*int main(void)
 {
+    char dest[] = "Happy birthday";
+    const char src[] = "to you";
+    size_t n;
+    
+    n = 8;
 
-
-
-
-  printf("%d\n",     );
-  return (0);
-}
+    ft_memcpy(dest, src, n);
+    printf ("%s\n", dest);
+    return (0);
+}*/
