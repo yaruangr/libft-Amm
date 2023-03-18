b519743a0139333a41cd45c761912d57e4ca0336
yaruangr <125121426+yaruangr@users.noreply.github.com>
2023-03-18 12:31:17 +0700

    finished

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yaruangr < yaruangr@student.42bangkok.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:49:39 by yaruangr          #+#    #+#             */
-/*   Updated: 2023/03/12 22:00:19 by yaruangr         ###   ########.fr       */
+/*   Updated: 2023/03/18 12:30:22 by yaruangr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,30 @@
 //#include    <stdio.h>
 //#include    <string.h>
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    unsigned char   *deststr;
-    unsigned char   *srcstr;
+	unsigned char	*deststr;
+	unsigned char	*srcstr;
 
-    deststr = dest;
-    srcstr = (unsigned char*)src;
-        if (!dest && !src)
-            return (NULL);
-        while (n-- > 0)
-        {
-            *deststr++ = *srcstr++;
-        }
-    return (dest);
+	deststr = dest;
+	srcstr = (unsigned char *)src;
+	if (!dest && !src)
+		return (NULL);
+	while (n-- > 0)
+	{
+		*deststr++ = *srcstr++;
+	}
+	return (dest);
 }
 
 /*int main(void)
 {
     char dest[] = "Happy birthday";
-    const char src[] = "to you";
-    size_t n;
-    
+	const char src[] = "to you";
+	size_t n;
     n = 8;
 
-    ft_memcpy(dest, src, n);
-    printf ("%s\n", dest);
-    return (0);
+	ft_memcpy(dest, src, n);
+	printf ("%s\n", dest);
+	return (0);
 }*/
